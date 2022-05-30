ddaba7c3e27996b29f0320a4de9336b353e5438c
devZzq <1962811421@qq.com>
2022-05-30 17:12:06 +0800

    .gitignore rewrite cache

--- demo-flyway/target/classes/db/migration/V20220601.1__user.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS `user`;
-
-CREATE TABLE `user` (
-                        `user_id` bigint(20) unsigned NOT NULL,
-                        `user_name` varchar(32) NOT NULL,
-                        `user_role_id` bigint(20) NOT NULL,
-                        PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
